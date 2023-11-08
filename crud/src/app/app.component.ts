ee9479a3b7aababa1c4adbcba38ac035626e0719
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-08 09:08:51 +0200

    frontend displays data from drf api

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  movies = [{title:'titanic'}, {title:'avatar',}, {title:'FAF',}, {title:'Test',}]
+  movies = [{ title: 'titanic' }, { title: 'avatar' }, { title: 'FAF' }, { title: 'Test' }];
+
+  constructor(private api: ApiService) {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# allow certain hosts to have access to my api(this django drf app)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:9000",
+]
+
 
 # Application definition
 
@@ -39,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'djangocrud.api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +56,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "djangocrud.urls"
