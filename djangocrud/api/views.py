0229c7f6dd9322864e88c3742a98af1c96af7636
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-09 08:25:41 +0200

    instead of returning ALL fields of a movie, return only two

--- djangocrud/api/views.py
+++ djangocrud/api/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import MovieSerializer
+from rest_framework.response import Response
+from .serializers import MovieMiniSerializer, MovieSerializer
 from .models import Movie
 
 
@@ -11,3 +12,10 @@ class MovieViewSet(viewsets.ModelViewSet):
     """
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        """We are using a build in list function instead, we say that we don't want to use
+        the default MovieViewSet, but our custom one instead."""
+        movies = Movie.objects.all()
+        serializer = MovieMiniSerializer(movies, many=True)
+        return Response(serializer.data)
